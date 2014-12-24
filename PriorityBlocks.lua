0ae86ac945671b280881f29dad4f0de1269a920b
Hunter Sadler <HSadlersemail@gmail.com>
2014-12-24 00:15:26 -0600

    Show Popup On Load

--- PriorityBlocks.lua
+++ PriorityBlocks.lua
@@ -1,3 +1,16 @@
 function PriorityBlocks_OnLoad()
-  
+    StaticPopupDialogs["firstTime_PriorityBlocks"] = {
+      text = "Welcome To Priority Blocks",
+      button1 = "Okay",
+      OnAccept = function()
+
+      end,
+      timeout = 0,
+      whileDead = true,
+      hideOnEscape = true,
+      preferredIndex = 3,  -- avoid some UI taint, see http://www.wowace.com/announcements/how-to-avoid-some-ui-taint/
+  }
+
+
+  local firstShow = StaticPopup_Show("firstTime_PriorityBlocks")
 end
