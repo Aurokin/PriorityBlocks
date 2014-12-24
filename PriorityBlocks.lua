255e3ba6f0a46872f06966bde7474a63219d3791
Hunter Sadler <HSadlersemail@gmail.com>
2014-12-24 02:44:42 -0600

    Remove XML, Detect Addon Usage

--- PriorityBlocks.lua
+++ PriorityBlocks.lua
@@ -1,16 +1,29 @@
-function PriorityBlocks_OnLoad()
-    StaticPopupDialogs["firstTime_PriorityBlocks"] = {
-      text = "Welcome To Priority Blocks",
-      button1 = "Okay",
-      OnAccept = function()
-
-      end,
-      timeout = 0,
-      whileDead = true,
-      hideOnEscape = true,
-      preferredIndex = 3,  -- avoid some UI taint, see http://www.wowace.com/announcements/how-to-avoid-some-ui-taint/
-  }
-
-
-  local firstShow = StaticPopup_Show("firstTime_PriorityBlocks")
+local function eventHandler(self, event, ...)
+  if event == "ADDON_LOADED" then
+    if PriorityBlocks_Loaded == nil then
+      PriorityBlocks_Loaded = 1;
+      local firstShow = StaticPopup_Show("firstTime_PriorityBlocks")
+      print("Welcome To Priority Blocks");
+    end
+  end
 end
+
+
+-- Register Frames
+local frame = CreateFrame("Frame", "PriorityButtonsFrame", UIParent);
+-- Register Events
+frame:RegisterEvent("ADDON_LOADED");
+-- Addon PopupDialogs
+StaticPopupDialogs["firstTime_PriorityBlocks"] = {
+  text = "Welcome To Priority Blocks",
+  button1 = "Okay",
+  OnAccept = function()
+
+  end,
+  timeout = 0,
+  whileDead = true,
+  hideOnEscape = true,
+  preferredIndex = 3,  -- avoid some UI taint, see http://www.wowace.com/announcements/how-to-avoid-some-ui-taint/
+}
+-- Manage Events
+frame:SetScript("OnEvent", eventHandler);
