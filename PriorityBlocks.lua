d6ba91a412322bb7ea8a33f86f8c446c2c75683e
Hunter Sadler <HSadlersemail@gmail.com>
2014-12-24 22:29:08 -0600

    Show Parent Frame, Add Background

--- PriorityBlocks.lua
+++ PriorityBlocks.lua
@@ -1,18 +1,33 @@
 local function eventHandler(self, event, ...)
-  if event == "ADDON_LOADED" then
-    if PriorityBlocks_Loaded == nil then
-      PriorityBlocks_Loaded = 1;
-      local firstShow = StaticPopup_Show("firstTime_PriorityBlocks")
-      print("Welcome To Priority Blocks");
+  if ... == "PriorityBlocks" then
+    if event == "ADDON_LOADED" then
+      if PriorityBlocks_Loaded == nil then
+        PriorityBlocks_Loaded = 1;
+        local firstShow = StaticPopup_Show("firstTime_PriorityBlocks")
+        print("Welcome To Priority Blocks");
+      end
     end
   end
 end
 
+--
+pbMainHeight = 256;
+pbMainWidth = 256;
 
 -- Register Frames
 local frame = CreateFrame("Frame", "PriorityButtonsFrame", UIParent);
+frame.texture = frame:CreateTexture(nil,"BACKGROUND");
 -- Register Events
 frame:RegisterEvent("ADDON_LOADED");
+
+-- Set Frame Height/Width
+frame:SetHeight(pbMainHeight);
+frame:SetWidth(pbMainWidth);
+
+-- Give Frame Background
+frame.texture:SetAllPoints(frame)
+frame.texture:SetTexture(0.5, 0.5, 0.5, 0.5);
+
 -- Addon PopupDialogs
 StaticPopupDialogs["firstTime_PriorityBlocks"] = {
   text = "Welcome To Priority Blocks",
@@ -27,3 +42,8 @@ StaticPopupDialogs["firstTime_PriorityBlocks"] = {
 }
 -- Manage Events
 frame:SetScript("OnEvent", eventHandler);
+
+-- Show Main Container Frame
+print("Hello World");
+frame:SetPoint("CENTER", 0, 0);
+frame:Show();
